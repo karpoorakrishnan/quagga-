de5aa982cb9f19f27e760951a1a32a802efae612
karpoorakrishnan <kkrishnan@techelium.com>
2016-12-07 15:26:03 +0530

    zebra

--- test_main.c
+++ test_main.c
@@ -1,7 +1,5 @@
-/* zebra daemon main routine.
+/* main routine.
  * Copyright (C) 1997, 98 Kunihiro Ishiguro
- *
- * This file is part of GNU Zebra.
  *
  * GNU Zebra is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -29,7 +27,6 @@
 #include "memory.h"
 #include "prefix.h"
 #include "log.h"
-#include "plist.h"
 #include "privs.h"
 #include "sigevent.h"
 #include "vrf.h"
@@ -38,9 +35,7 @@
 #include "zebra/zserv.h"
 #include "zebra/debug.h"
 #include "zebra/router-id.h"
-#include "zebra/irdp.h"
-#include "zebra/rtadv.h"
-#include "zebra/zebra_fpm.h"
+#include "zebra/interface.h"
 
 /* Zebra instance */
 struct zebra_t zebrad =
@@ -51,41 +46,23 @@ struct zebra_t zebrad =
 /* process id. */
 pid_t pid;
 
+/* zebra_rib's workqueue hold time. Private export for use by test code only */
+extern int rib_process_hold_time;
+
 /* Pacify zclient.o in libzebra, which expects this variable. */
 struct thread_master *master;
-
-/* Route retain mode flag. */
-int retain_mode = 0;
-
-/* Don't delete kernel route. */
-int keep_kernel_mode = 0;
-
-#ifdef HAVE_NETLINK
-/* Receive buffer size for netlink socket */
-u_int32_t nl_rcvbufsize = 0;
-#endif /* HAVE_NETLINK */
 
 /* Command line options. */
 struct option longopts[] = 
 {
   { "batch",       no_argument,       NULL, 'b'},
   { "daemon",      no_argument,       NULL, 'd'},
-  { "keep_kernel", no_argument,       NULL, 'k'},
-  { "fpm_format",  required_argument, NULL, 'F'},
   { "config_file", required_argument, NULL, 'f'},
-  { "pid_file",    required_argument, NULL, 'i'},
-  { "socket",      required_argument, NULL, 'z'},
   { "help",        no_argument,       NULL, 'h'},
   { "vty_addr",    required_argument, NULL, 'A'},
   { "vty_port",    required_argument, NULL, 'P'},
-  { "retain",      no_argument,       NULL, 'r'},
-  { "dryrun",      no_argument,       NULL, 'C'},
-#ifdef HAVE_NETLINK
-  { "nl-bufsize",  required_argument, NULL, 's'},
-#endif /* HAVE_NETLINK */
-  { "user",        required_argument, NULL, 'u'},
-  { "group",       required_argument, NULL, 'g'},
   { "version",     no_argument,       NULL, 'v'},
+  { "rib_hold",	   required_argument, NULL, 'r'},
   { 0 }
 };
 
@@ -94,21 +71,6 @@ zebra_capabilities_t _caps_p [] =
   ZCAP_NET_ADMIN,
   ZCAP_SYS_ADMIN,
   ZCAP_NET_RAW,
-};
-
-/* zebra privileges to run with */
-struct zebra_privs_t zserv_privs =
-{
-#if defined(QUAGGA_USER) && defined(QUAGGA_GROUP)
-  .user = QUAGGA_USER,
-  .group = QUAGGA_GROUP,
-#endif
-#ifdef VTY_GROUP
-  .vty_group = VTY_GROUP,
-#endif
-  .caps_p = _caps_p,
-  .cap_num_p = array_size(_caps_p),
-  .cap_num_i = 0
 };
 
 /* Default configuration file path. */
@@ -131,28 +93,62 @@ usage (char *progname, int status)
 	      "-b, --batch        Runs in batch mode\n"\
 	      "-d, --daemon       Runs in daemon mode\n"\
 	      "-f, --config_file  Set configuration file name\n"\
-	      "-F, --fpm_format   Set fpm format to 'netlink' or 'protobuf'\n"\
-	      "-i, --pid_file     Set process identifier file name\n"\
-	      "-z, --socket       Set path of zebra socket\n"\
-	      "-k, --keep_kernel  Don't delete old routes which installed by "\
-				  "zebra.\n"\
-	      "-C, --dryrun       Check configuration for validity and exit\n"\
 	      "-A, --vty_addr     Set vty's bind address\n"\
 	      "-P, --vty_port     Set vty's port number\n"\
-	      "-r, --retain       When program terminates, retain added route "\
-				  "by zebra.\n"\
-	      "-u, --user         User to run as\n"\
-	      "-g, --group	  Group to run as\n", progname);
-#ifdef HAVE_NETLINK
-      printf ("-s, --nl-bufsize   Set netlink receive buffer size\n");
-#endif /* HAVE_NETLINK */
-      printf ("-v, --version      Print program version\n"\
+	      "-r, --rib_hold	  Set rib-queue hold time\n"\
+              "-v, --version      Print program version\n"\
 	      "-h, --help         Display this help and exit\n"\
 	      "\n"\
-	      "Report bugs to %s\n", ZEBRA_BUG_ADDRESS);
+	      "Report bugs to %s\n", progname, ZEBRA_BUG_ADDRESS);
     }
 
   exit (status);
+}
+
+static ifindex_t test_ifindex = 0;
+
+/* testrib commands */
+DEFUN (test_interface_state,
+       test_interface_state_cmd,
+       "state (up|down)",
+       "configure interface\n"
+       "up\n"
+       "down\n")
+{
+  struct interface *ifp;
+  if (argc < 1)
+    return CMD_WARNING;
+  
+  ifp = vty->index;
+  if (ifp->ifindex == IFINDEX_INTERNAL)
+    {
+      ifp->ifindex = ++test_ifindex;
+      ifp->mtu = 1500;
+      ifp->flags = IFF_BROADCAST|IFF_MULTICAST;
+    }
+  
+  switch (argv[0][0])
+    {
+      case 'u':
+        SET_FLAG (ifp->flags, IFF_UP);
+        if_add_update (ifp);
+        printf ("up\n");
+        break;
+      case 'd':
+        UNSET_FLAG (ifp->flags, IFF_UP);
+        if_delete_update (ifp);
+        printf ("down\n");
+        break;
+      default:
+        return CMD_WARNING;
+    }
+  return CMD_SUCCESS;
+}
+
+static void
+test_cmd_init (void)
+{
+  install_element (INTERFACE_NODE, &test_interface_state_cmd);
 }
 
 /* SIGHUP handler. */
@@ -170,12 +166,6 @@ static void
 sigint (void)
 {
   zlog_notice ("Terminating on signal");
-
-  if (!retain_mode)
-    rib_close ();
-#ifdef HAVE_IRDP
-  irdp_finish();
-#endif
 
   exit (0);
 }
@@ -217,7 +207,6 @@ zebra_vrf_new (vrf_id_t vrf_id, void **info)
     {
       zvrf = zebra_vrf_alloc (vrf_id);
       *info = (void *)zvrf;
-      router_id_init (zvrf);
     }
 
   return 0;
@@ -231,11 +220,7 @@ zebra_vrf_enable (vrf_id_t vrf_id, void **info)
 
   assert (zvrf);
 
-#if defined (HAVE_RTADV)
-  rtadv_init (zvrf);
-#endif
   kernel_init (zvrf);
-  interface_list (zvrf);
   route_read (zvrf);
 
   return 0;
@@ -262,13 +247,7 @@ zebra_vrf_disable (vrf_id_t vrf_id, void **info)
         if_down (ifp);
     }
 
-#if defined (HAVE_RTADV)
-  rtadv_terminate (zvrf);
-#endif
   kernel_terminate (zvrf);
-
-  list_delete_all_node (zvrf->rid_all_sorted_list);
-  list_delete_all_node (zvrf->rid_lo_sorted_list);
 
   return 0;
 }
@@ -289,15 +268,12 @@ main (int argc, char **argv)
 {
   char *p;
   char *vty_addr = NULL;
-  int vty_port = ZEBRA_VTY_PORT;
-  int dryrun = 0;
+  int vty_port = 0;
   int batch_mode = 0;
   int daemon_mode = 0;
   char *config_file = NULL;
   char *progname;
   struct thread thread;
-  char *zserv_path = NULL;
-  char *fpm_format = NULL;
 
   /* Set umask before anything for security */
   umask (0027);
@@ -312,11 +288,7 @@ main (int argc, char **argv)
     {
       int opt;
   
-#ifdef HAVE_NETLINK  
-      opt = getopt_long (argc, argv, "bdkf:F:i:z:hA:P:ru:g:vs:C", longopts, 0);
-#else
-      opt = getopt_long (argc, argv, "bdkf:F:i:z:hA:P:ru:g:vC", longopts, 0);
-#endif /* HAVE_NETLINK */
+      opt = getopt_long (argc, argv, "bdf:hA:P:r:v", longopts, 0);
 
       if (opt == EOF)
 	break;
@@ -330,26 +302,11 @@ main (int argc, char **argv)
 	case 'd':
 	  daemon_mode = 1;
 	  break;
-	case 'k':
-	  keep_kernel_mode = 1;
-	  break;
-	case 'C':
-	  dryrun = 1;
-	  break;
 	case 'f':
 	  config_file = optarg;
 	  break;
-	case 'F':
-	  fpm_format = optarg;
-	  break;
 	case 'A':
 	  vty_addr = optarg;
-	  break;
-        case 'i':
-          pid_file = optarg;
-          break;
-	case 'z':
-	  zserv_path = optarg;
 	  break;
 	case 'P':
 	  /* Deal with atoi() returning 0 on failure, and zebra not
@@ -360,22 +317,9 @@ main (int argc, char **argv)
 	      break;
 	    } 
 	  vty_port = atoi (optarg);
-	  if (vty_port <= 0 || vty_port > 0xffff)
-	    vty_port = ZEBRA_VTY_PORT;
 	  break;
 	case 'r':
-	  retain_mode = 1;
-	  break;
-#ifdef HAVE_NETLINK
-	case 's':
-	  nl_rcvbufsize = atoi (optarg);
-	  break;
-#endif /* HAVE_NETLINK */
-	case 'u':
-	  zserv_privs.user = optarg;
-	  break;
-	case 'g':
-	  zserv_privs.group = optarg;
+	  rib_process_hold_time = atoi(optarg);
 	  break;
 	case 'v':
 	  print_version (progname);
@@ -389,66 +333,37 @@ main (int argc, char **argv)
 	  break;
 	}
     }
-
+  
+  /* port and conf file mandatory */
+  if (!vty_port || !config_file)
+    {
+      fprintf (stderr, "Error: --vty_port and --config_file arguments"
+                       " are both required\n");
+      usage (progname, 1);
+    }
+  
   /* Make master thread emulator. */
   zebrad.master = thread_master_create ();
-
-  /* privs initialise */
-  zprivs_init (&zserv_privs);
 
   /* Vty related initialize. */
   signal_init (zebrad.master, array_size(zebra_signals), zebra_signals);
   cmd_init (1);
   vty_init (zebrad.master);
   memory_init ();
+  zebra_debug_init ();
+  zebra_if_init ();
+  test_cmd_init ();
 
   /* Zebra related initialize. */
-  zebra_init ();
   rib_init ();
-  zebra_if_init ();
-  zebra_debug_init ();
-  router_id_cmd_init ();
-  zebra_vty_init ();
   access_list_init ();
-  prefix_list_init ();
-#if defined (HAVE_RTADV)
-  rtadv_cmd_init ();
-#endif
-#ifdef HAVE_IRDP
-  irdp_init();
-#endif
 
-  /* For debug purpose. */
-  /* SET_FLAG (zebra_debug_event, ZEBRA_DEBUG_EVENT); */
-
-  /* Initialize VRF module, and make kernel routing socket. */
+  /* Make kernel routing socket. */
   zebra_vrf_init ();
+  zebra_vty_init();
 
-#ifdef HAVE_SNMP
-  zebra_snmp_init ();
-#endif /* HAVE_SNMP */
-
-#ifdef HAVE_FPM
-  zfpm_init (zebrad.master, 1, 0, fpm_format);
-#else
-  zfpm_init (zebrad.master, 0, 0, fpm_format);
-#endif
-
-  /* Process the configuration file. Among other configuration
-  *  directives we can meet those installing static routes. Such
-  *  requests will not be executed immediately, but queued in
-  *  zebra->ribq structure until we enter the main execution loop.
-  *  The notifications from kernel will show originating PID equal
-  *  to that after daemon() completes (if ever called).
-  */
+  /* Configuration file read*/
   vty_read_config (config_file, config_default);
-
-  /* Don't start execution if we are in dry-run mode */
-  if (dryrun)
-    return(0);
-
-  /* Count up events for interfaces */
-  if_startup_count_up ();
 
   /* Clean up rib. */
   rib_weed_tables ();
@@ -460,32 +375,15 @@ main (int argc, char **argv)
   /* Daemonize. */
   if (daemon_mode && daemon (0, 0) < 0)
     {
-      zlog_err("Zebra daemon failed: %s", strerror(errno));
+      perror("daemon start failed");
       exit (1);
     }
-
-  /* Output pid of zebra. */
-  pid_output (pid_file);
-
-  /* After we have successfully acquired the pidfile, we can be sure
-  *  about being the only copy of zebra process, which is submitting
-  *  changes to the FIB.
-  *  Clean up zebra-originated routes. The requests will be sent to OS
-  *  immediately, so originating PID in notifications from kernel
-  *  will be equal to the current getpid(). To know about such routes,
-  * we have to have route_read() called before.
-  */
-  if (! keep_kernel_mode)
-    rib_sweep_route ();
 
   /* Needed for BSD routing socket. */
   pid = getpid ();
 
-  /* This must be done only after locking pidfile (bug #403). */
-  zebra_zserv_socket_init (zserv_path);
-
   /* Make vty server socket. */
-  vty_serv_sock (vty_addr, vty_port, ZEBRA_VTYSH_PATH);
+  vty_serv_sock (vty_addr, vty_port, "/tmp/test_zebra");
 
   /* Print banner. */
   zlog_notice ("Zebra %s starting: vty@%d", QUAGGA_VERSION, vty_port);
